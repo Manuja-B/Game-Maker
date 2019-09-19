4ab25d42d1d319ce8942c3dc5145db32da1a1a9c
bivas <bmaiti@iu.edu>
2019-09-19 19:36:31 -0400

    created boundary behavior interface

--- src/com/oosd/gamemaker/behavior/AutomaticMovement.java
+++ src/com/oosd/gamemaker/behavior/AutomaticMovement.java
@@ -9,20 +9,10 @@ public class AutomaticMovement implements Movement {
 	@Override
 	public void move(Sprite sprite, JPanel playground) {
 		// TODO Auto-generated method stub
-		int xMin = sprite.getX() - sprite.getWidth()/2;
-        int yMin = sprite.getY() - sprite.getHeight()/2;
-        int xMax = sprite.getX() + sprite.getWidth()/2;
-        int yMax = sprite.getY() + sprite.getHeight()/2;;
-        System.out.println(playground.getWidth());
-        if (xMin < 0 || xMax > playground.getWidth()) {
-            sprite.setDx(-sprite.getDx());
-        }
-        if (yMin < 0 || yMax > playground.getHeight()) {
-        	sprite.setDy(-sprite.getDy());
-        }
+		
         int positionX = sprite.getX() + sprite.getDx();
         int positionY = sprite.getY()+ sprite.getDy();
         sprite.update(positionX, positionY);
 	}
-	
+
 }
---
+++ src/com/oosd/gamemaker/behavior/BoundaryBehavior.java
@@ -0,0 +1,8 @@
+package com.oosd.gamemaker.behavior;
+import com.oosd.gamemaker.Playground;
+import com.oosd.gamemaker.models.Sprite;
+
+public interface BoundaryBehavior {
+	public abstract void reactWithBoundary(Sprite sprite, Playground playground);
+}
+
