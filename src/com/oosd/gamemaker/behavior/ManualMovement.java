c746f2386f24a40014208e233bd28bf8d2ae6018
unknown <virendra.wali@gmail.com>
2019-10-03 13:28:03 -0400

    https://test.uisapp2.iu.edu/jira-stg/browse/P532F19T2-56 https://test.uisapp2.iu.edu/jira-stg/browse/P532F19T2-57

--- src/com/oosd/gamemaker/behavior/ManualMovement.java
+++ src/com/oosd/gamemaker/behavior/ManualMovement.java
@@ -1,71 +1,105 @@
 package com.oosd.gamemaker.behavior;
 
+import java.awt.KeyEventDispatcher;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import javax.swing.JPanel;
 
 import com.oosd.gamemaker.models.Sprite;
 
-public class ManualMovement implements Movement, KeyListener{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -3233933797707271155L;
-	int dx;
-	int positionX;
-	int positionY;
-	boolean isLeft;
-	boolean isRight;
+public class ManualMovement implements Movement, KeyEventDispatcher {
 
+	private static final long serialVersionUID = 8062557088943136222L;
+	
+	private Boolean isLeft = false;
+	private Boolean isRight = false;
+	private Boolean isDown = false;
+	private Boolean isUp = false;
+	
+	private Boolean isLeftCode = false;
+	private Boolean isRightCode = false;
+	private Boolean isDownCode = false;
+	private Boolean isUpCode = false;
+
+	int key;
+	
+	public ManualMovement(int key, int directionCode) {
+		super();
+		this.key = key;
+		switch(directionCode)
+		{
+			case(0):
+				this.isUpCode = true;
+				break;
+			case(1):
+				this.isDownCode = true;
+				break;
+			case(2):
+				this.isLeftCode = true;
+				break;
+			case(3):
+				this.isRightCode = true;
+				break;
+			default:
+				System.out.println("No code is set");
+		}
+	}
+	
 	@Override
 	public void move(Sprite sprite, JPanel playground) {
-        int positionY = sprite.getY();;
-		int positionX = sprite.getX();;
-		int maxX = playground.getWidth();
-		dx = sprite.getDx();
+		int positionY = sprite.getY();
+		int positionX = sprite.getX();
+		
+		int maxRight = playground.getWidth();
+		int endpositionX = positionX + sprite.getWidth();
+		
+		int endpositionY = positionY + sprite.getHeight();
+		
+		int maxY = playground.getHeight()-220;
+		
+		int dx = 1;
+		int dy = 1;
+		
+		
 		if (isLeft && positionX > 0) {
 			positionX -= dx;
 		}
-		if (isRight && (positionX + (sprite.getWidth())) < maxX) {
+		else if (isRight && endpositionX < maxRight) {
 			positionX += dx;
+		} 
+		else if (isDown && endpositionY < maxY) {
+			positionY += dy;
 		}
+		else if(isUp && positionY > 0) {
+			positionY -= dy;
+		}
+		
 		sprite.update(positionX, positionY);
-		
-}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
-		System.out.println("press");
-		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-
-			this.isLeft = true;
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		if(e.getKeyCode() == key) {
+			if (e.getID() == KeyEvent.KEY_PRESSED && isLeftCode) {
+	            this.isLeft = true;
+	        } else if (e.getID() == KeyEvent.KEY_RELEASED && isLeftCode) {
+	            this.isLeft = false;
+	        } else if (e.getID() == KeyEvent.KEY_PRESSED && isRightCode) {
+	            this.isRight = true;
+	        } else if (e.getID() == KeyEvent.KEY_RELEASED && isRightCode) {
+	            this.isRight = false;
+	        } else if (e.getID() == KeyEvent.KEY_PRESSED && isDownCode) {
+	            this.isDown = true;
+	        } else if (e.getID() == KeyEvent.KEY_RELEASED && isDownCode) {
+	            this.isDown = false;
+	        }
+			else if (e.getID() == KeyEvent.KEY_PRESSED && isUpCode) {
+	            this.isUp = true;
+	        } else if (e.getID() == KeyEvent.KEY_RELEASED && isUpCode) {
+	            this.isUp = false;
+	        }
 		}
-
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-
-			this.isRight = true;
-
-		}
+		return false;
 	}
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 
-			this.isLeft = false;
-		}
-
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-
-			this.isRight = false;
-
-		}
-		
-	}
 }
