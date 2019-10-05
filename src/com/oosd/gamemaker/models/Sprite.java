fa960894c08195b5f063c3d2611c1a99c0c0c2f7
unknown <virendra.wali@gmail.com>
2019-10-05 16:25:31 -0400

    removed stale code

--- src/com/oosd/gamemaker/models/Sprite.java
+++ src/com/oosd/gamemaker/models/Sprite.java
@@ -19,10 +19,6 @@ public abstract class Sprite implements Serializable{
 	private boolean isGamePaused = true;
 	private int dx = 1;
 	private int dy = 1;
-
-	private int previousDx = 1;
-	private int previousDy = 1;
-	
 	protected int locationX,locationY;
 	protected String name;
 	protected int height, width;
@@ -99,12 +95,10 @@ public abstract class Sprite implements Serializable{
 		return dy;
 	}
 	public void setDx(int dx) {
-		this.previousDx = this.dx;
 		this.dx = dx;
 	}
 	
 	public void setDy(int dy) {
-		this.previousDy = this.dy;
 		this.dy = dy;
 	}
 	
@@ -178,19 +172,6 @@ public abstract class Sprite implements Serializable{
 	}
 	public void setShape(Shape shape) {
 		this.shape = shape;
-	}
-	
-	public int getPreviousDx() {
-		return previousDx;
-	}
-	public void setPreviousDx(int previousDx) {
-		this.previousDx = previousDx;
-	}
-	public int getPreviousDy() {
-		return previousDy;
-	}
-	public void setPreviousDy(int previousDy) {
-		this.previousDy = previousDy;
 	}
 
 }
